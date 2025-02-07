51a86ceec05b3d538a17100448438d49b7ade325
ilsyaa <ilham.shindou@gmail.com>
2025-02-07 17:17:14 +0700

    feat: edit filter search & sorting icon list
    Ubah logika filter dan urutan ikon pada pencarian.
    - Penambahan bobot pada keyword pencocokan.
    - Pengurutan berdasarkan bobot pencocokan.

--- app/api/data/route.js
+++ app/api/data/route.js
@@ -9,23 +9,27 @@ export async function GET(req) {
 
     const jsonData = fs.readFileSync(path.join(process.cwd(), 'data', 'icons.json'));
     const data = JSON.parse(jsonData);
-    let filteredIcons = []
+    let filteredIcons = [];
 
     if (search) {
-        filteredIcons = Object.entries(data).filter(
-            ([key, value]) => {
-                return key.toLowerCase().includes(search.toLowerCase()) ||
-                    value.label.toLowerCase().includes(search.toLowerCase()) ||
-                    value.search.terms.some(term => term.toLowerCase().includes(search.toLowerCase()));
+        filteredIcons = Object.entries(data).map(([key, value]) => {
+            let score = 0;
+            if (key.toLowerCase().includes(search.toLowerCase())) {
+                score += 3;
             }
-        )
+            if (value.label.toLowerCase().includes(search.toLowerCase())) {
+                score += 2;
+            }
+            const termMatches = value.search.terms.filter(term => term.toLowerCase().includes(search.toLowerCase()));
+            score += termMatches.length;
+            return [key, { ...value, score }];
+        });
+        filteredIcons = filteredIcons.filter(([key, value]) => value.score > 0);
+        filteredIcons.sort((a, b) => b[1].score - a[1].score);
     } else {
         filteredIcons = Object.entries(data);
+        filteredIcons.sort((a, b) => b[1].voted - a[1].voted);
     }
-
-    filteredIcons.sort((a, b) => {
-        return b[1].voted - a[1].voted;
-    });
 
     const startIndex = (page - 1) * limit;
     const endIndex = startIndex + limit;
